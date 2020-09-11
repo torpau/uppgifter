3e21651feb140e5bf56a3a51ecae89b989888d21
Torsten Pause <torsten.pause@student.kyh.se>
2020-09-11 11:13:00 +0200

    task 11

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	    //uppgifter.uppg9();
-
-        uppgifter.uppg10();
+        //uppgifter.uppg10();
+        uppgifter.uppg11();
     }
 }
--- src/com/company/uppgifter.java
+++ src/com/company/uppgifter.java
@@ -6,25 +6,28 @@ public class uppgifter {
     static void uppg9() {
         double result = ((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));
         System.out.println(result);
+        // Expected Output 2.138888888888889
     }
-/*
-Test data:
-((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5))
-Expected Output
-2.138888888888889
-*/
 
 
     static void uppg10() {
         double result = 4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11));
         System.out.println(result);
+        //Expected Output 2.9760461760461765
     }
-/*
-Specified Formula :
-4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11))
-Expected Output
-2.9760461760461765
-*/
 
 
+    static void uppg11() {
+        double R = 7.5;
+        double P = Math.PI * (2*R);
+        double A = Math.PI * Math.pow(R,2);
+
+        System.out.println(P);
+        System.out.println(A);
+        /*
+        Expected Output
+        Perimeter is = 47.12388980384689
+        Area is = 176.71458676442586
+        */
+    }
 }
