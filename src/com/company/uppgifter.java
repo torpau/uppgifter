55d4a2282a7c321adff927095ae9dd84b1e24254
Torsten Pause <torsten.pause@student.kyh.se>
2020-09-11 10:57:55 +0200

    task 9 & 10

---
+++ src/com/company/uppgifter.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class uppgifter {
+    //https://www.w3resource.com/java-exercises/basic/index.php
+
+    static void uppg9() {
+        double result = ((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));
+        System.out.println(result);
+    }
+/*
+Test data:
+((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5))
+Expected Output
+2.138888888888889
+*/
+
+
+    static void uppg10() {
+        double result = 4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11));
+        System.out.println(result);
+    }
+/*
+Specified Formula :
+4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11))
+Expected Output
+2.9760461760461765
+*/
+
+
+}
