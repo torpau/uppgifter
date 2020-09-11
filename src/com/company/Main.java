2006cb5a5ac10247b0658d5c8a018a1fe4142f6a
Torsten <71069908+torpau@users.noreply.github.com>
2020-09-11 14:01:41 +0200

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,9 +7,9 @@ public class Main {
         // uppgifter.uppg10();
         // uppgifter.uppg11();
         // uppgifter.uppg12();
-        //uppgifter.uppg13();
-        //uppgifter.uppg14();
-        //uppgifter.uppg15();
+        // uppgifter.uppg13();
+        // uppgifter.uppg14();
+        // uppgifter.uppg15();
         uppgifter.uppg16();
     }
 }
